6752a0f30c580a6409659940bfb8b7e121686826
John Sandoval <jcsandoval978@gmail.com>
2024-01-26 16:08:25 -0500

    Update password hashing library

--- server/src/main/java/sandoval/john/server/services/UserService.java
+++ server/src/main/java/sandoval/john/server/services/UserService.java
@@ -2,7 +2,7 @@ package sandoval.john.server.services;
 
 import java.util.Optional;
 
-import org.springframework.security.crypto.bcrypt.BCrypt;
+import org.mindrot.jbcrypt.BCrypt;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.validation.BindingResult;
